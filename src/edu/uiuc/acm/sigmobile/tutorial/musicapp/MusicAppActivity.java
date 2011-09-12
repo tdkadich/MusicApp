976a0dc2f2bc5411c1078bb3dcf43b5028206698
Timothy D. Kadich <timtim@timtim-laptop.(none)>
2011-09-11 19:56:34 -0500

    Helllooooooo!

---
+++ src/edu/uiuc/acm/sigmobile/tutorial/musicapp/MusicAppActivity.java
@@ -0,0 +1,28 @@
+package edu.uiuc.acm.sigmobile.tutorial.musicapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MusicAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        final Button bShowSongs = (Button) findViewById(R.id.show_songs);
+        bShowSongs.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				bShowSongs.setText("pressed!");
+			}
+		});
+    }
+    
+    
+    
+}
